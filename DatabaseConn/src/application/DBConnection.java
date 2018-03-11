e3f7587a00622c3062dabf3e3cadea08c2aea68e
mihap <mihap@192.168.1.4>
2018-03-11 13:48:53 +0100

    ttt

---
+++ DatabaseConn/src/application/DBConnection.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3316/world", "root", "Admin9660");
+		System.out.println("Connected to DB");
+	}
+	
+	
+}
